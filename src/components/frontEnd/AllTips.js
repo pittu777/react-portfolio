281a4eb5911ae5345233adebd92c2bad46ed6fbd
pittu777 <pittuprasanth98@gmail.com>
2024-06-21 18:07:45 +0530

    up2

--- src/components/frontEnd/AllTips.js
+++ src/components/frontEnd/AllTips.js
@@ -1,6 +1,14 @@
 import React from "react";
 import {
   tipsLog1,
+  tipsLog10,
+  tipsLog11,
+  tipsLog12,
+  tipsLog13,
+  tipsLog14,
+  tipsLog15,
+  tipsLog16,
+  tipsLog17,
   tipsLog2,
   tipsLog3,
   tipsLog4,
@@ -8,6 +16,7 @@ import {
   tipsLog6,
   tipsLog7,
   tipsLog8,
+  tipsLog9,
 } from "./logoImports";
 import "./index.scss";
 
@@ -32,7 +41,7 @@ const AllTips = () => {
             alt="javascript"
           />
         </a>
-        <a href="/reactTips" rel="noreferrer">
+        <a href="/angularTips" rel="noreferrer">
           <img
             className="small-image"
             rel="tips-logo"
@@ -40,7 +49,7 @@ const AllTips = () => {
             alt="javascript"
           />
         </a>
-        <a href="/cssTips" rel="noreferrer">
+        <a href="/angularTips" rel="noreferrer">
           <img
             className="small-image"
             rel="tips-logo"
@@ -48,8 +57,7 @@ const AllTips = () => {
             alt="javascript"
           />
         </a>
-
-        <a href="/rxjsTips" rel="noreferrer">
+        <a href="/angularTips" rel="noreferrer">
           <img
             className="small-image"
             rel="tips-logo"
@@ -57,7 +65,7 @@ const AllTips = () => {
             alt="javascript"
           />
         </a>
-        <a href="typescriptTips" rel="noreferrer">
+        <a href="/angularTips" rel="noreferrer">
           <img
             className="small-image"
             rel="tips-logo"
@@ -65,7 +73,7 @@ const AllTips = () => {
             alt="javascript"
           />
         </a>
-        <a href="typescriptTips" rel="noreferrer">
+        <a href="/angularTips" rel="noreferrer">
           <img
             className="small-image"
             rel="tips-logo"
@@ -73,7 +81,7 @@ const AllTips = () => {
             alt="javascript"
           />
         </a>
-        <a href="typescriptTips" rel="noreferrer">
+        <a href="/angularTips" rel="noreferrer">
           <img
             className="small-image"
             rel="tips-logo"
@@ -81,7 +89,81 @@ const AllTips = () => {
             alt="javascript"
           />
         </a>
+        <a href="/angularTips" rel="noreferrer">
+          <img
+            className="small-image"
+            rel="tips-logo"
+            src={tipsLog9}
+            alt="javascript"
+          />
+        </a>
+
+        <a href="/angularTips" rel="noreferrer">
+          <img
+            className="small-image"
+            rel="tips-logo"
+            src={tipsLog10}
+            alt="javascript"
+          />
+        </a>
         
+        <a href="/angularTips" rel="noreferrer">
+          <img
+            className="small-image"
+            rel="tips-logo"
+            src={tipsLog11}
+            alt="javascript"
+          />
+        </a>
+        <a href="/angularTips" rel="noreferrer">
+          <img
+            className="small-image"
+            rel="tips-logo"
+            src={tipsLog12}
+            alt="javascript"
+          />
+        </a>
+        <a href="/angularTips" rel="noreferrer">
+          <img
+            className="small-image"
+            rel="tips-logo"
+            src={tipsLog13}
+            alt="javascript"
+          />
+        </a>
+        <a href="/angularTips" rel="noreferrer">
+          <img
+            className="small-image"
+            rel="tips-logo"
+            src={tipsLog14}
+            alt="javascript"
+          />
+        </a>
+        
+        <a href="/angularTips" rel="noreferrer">
+          <img
+            className="small-image"
+            rel="tips-logo"
+            src={tipsLog15}
+            alt="javascript"
+          />
+        </a>
+        <a href="/angularTips" rel="noreferrer">
+          <img
+            className="small-image"
+            rel="tips-logo"
+            src={tipsLog16}
+            alt="javascript"
+          />
+        </a>
+        <a href="/angularTips" rel="noreferrer">
+          <img
+            className="small-image"
+            rel="tips-logo"
+            src={tipsLog17}
+            alt="javascript"
+          />
+        </a>
       </div>
     </>
   );
